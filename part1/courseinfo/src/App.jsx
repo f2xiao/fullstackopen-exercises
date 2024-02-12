4fdf71cf3b8b8f109f76278d8733c9f45016a266
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-02-12 17:03:31 -0500

    refactor the components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -35,6 +35,7 @@ const Total = (props) => {
   }, 0);
 
   console.log( exercisesTotal );
+  
   return (
     <>
       <p>Number of exercises {exercisesTotal}</p>
@@ -45,30 +46,33 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development';
 
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    } 
+  const course = {
+    name: 'Half Stack application development', 
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      } 
+  
+    ]
 
-  ];
+  }
 
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
